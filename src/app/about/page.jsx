9d7c05b4ec46cfda9a6b06bac9aba9176b4c329d
megzzawy <amgadsameh99@gmail.com>
2025-04-01 19:37:17 +0200

    create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+export default function page ()  {
+  return (
+    <div className='p-5 max-w-7xl mx-auto space-y-4'>
+      <h1 className='text-2xl text-amber-600 '>About</h1>
+      <p>Pagodas, with their elegant tiered roofs and intricate designs, have long stood as symbols of spiritual reverence and architectural brilliance. Originating in ancient Asia, these towering structures were often built as sacred monuments, housing relics or serving as places of meditation. Each tier of a pagoda represents a step closer to enlightenment, a physical manifestation of the journey toward wisdom and inner peace.</p>
+      <p>The soft chime of wind bells dangling from the curved eaves fills the air, creating a melody that seems to whisper ancient secrets. Surrounding gardens, often adorned with cherry blossoms or tranquil ponds, enhance the pagoda’s serene aura. Whether bathed in the golden light of dawn or silhouetted against a twilight sky, pagodas remain timeless guardians of culture, history, and tranquility.</p>
+      <p>Pagodas, with their elegant tiered roofs and intricate designs, have long stood as symbols of spiritual reverence and architectural brilliance. Originating in ancient Asia, these towering structures were often built as sacred monuments, housing relics or serving as places of meditation. Each tier of a pagoda represents a step closer to enlightenment, a physical manifestation of the journey toward wisdom and inner peace.</p>
+    </div>
+  )
+}
